9ad13efe6fced4ba866e1352db2d7af76ac2b59d
shun198 <shunhiroseluvmri@gmail.com>
2023-05-12 06:53:19 +0900

    👍 update environment variables

--- application/project/settings/environment.py
+++ application/project/settings/environment.py
@@ -6,11 +6,11 @@ from pydantic import BaseSettings
 class DjangoSettings(BaseSettings):
     """Django関連の環境変数を設定するクラス"""
 
-    SECRET_KEY: str
-    ALLOWED_HOSTS: str
-    POSTGRES_NAME: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
+    SECRET_KEY: str = "test"
+    ALLOWED_HOSTS: str = "localhost 127.0.0.1 [::1]"
+    POSTGRES_NAME: str = "postgres"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
     POSTGRES_HOST: str = "db"
     POSTGRES_PORT: int = 5432
 
@@ -20,10 +20,10 @@ class AwsSettings(BaseSettings):
 
     AWS_REGION_NAME: str = "ap-northeast-1"
     ENDPOINT_URL: str = "http://localstack:4566"
-    AWS_SES_REGION_NAME: str
-    AWS_SES_REGION_ENDPOINT: str
+    AWS_DEFAULT_REGION_NAME: str = "ap-northeast-1"
+    AWS_SES_REGION_ENDPOINT: str = "email.ap-northeast-1.amazonaws.com"
     AWS_STORAGE_BUCKET_NAME: str = "localstack"
-    DEFAULT_FROM_EMAIL: str
+    DEFAULT_FROM_EMAIL: str = "django@example.com"
     AWS_PROFILE: str = "localstack"
 
 
