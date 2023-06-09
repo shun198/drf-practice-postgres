c21c188e4cb8b166ed4a97801d4313061d93008c
shun198 <shunhiroseluvmri@gmail.com>
2023-06-09 21:38:44 +0900

    👍 add serializer

--- application/project/settings/environment.py
+++ application/project/settings/environment.py
@@ -18,7 +18,6 @@ class DjangoSettings(BaseSettings):
 class AwsSettings(BaseSettings):
     """AWS関連の環境変数を設定するクラス"""
 
-    AWS_REGION_NAME: str = "ap-northeast-1"
     ENDPOINT_URL: str = "http://localstack:4566"
     AWS_DEFAULT_REGION_NAME: str = "ap-northeast-1"
     AWS_SES_REGION_ENDPOINT: str = "email.ap-northeast-1.amazonaws.com"
