30c1611755d3f9a63cb7a3428552526ffa6abadb
shun198 <shunhiroseluvmri@gmail.com>
2023-06-10 21:38:54 +0900

    👍 edit to wrong result

--- application/application/tests/test_health_check.py
+++ application/application/tests/test_health_check.py
@@ -5,5 +5,5 @@ def test_health_check_returns_200(client):
     """ヘルスチェックで200を返すことをテスト"""
     url = "/api/health/"
     response = client.get(url, format="json")
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {"msg": "pass"}
